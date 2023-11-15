730b0a0152d38769af45b6c95e7741e3330b000d
jalisastewart <jalisastewart55@gmail.com>
2023-11-14 19:58:41 -0600

    Added extraction code

---
+++ etl.py
@@ -0,0 +1,20 @@
+""" Python Extraction """
+
+
+# Import python packages
+
+import requests
+import pandas as pd
+from sqlalchemy import create_engine
+
+# Extract
+
+def extract()-> dict:
+    # This API extracts data from http://universities.hipolabs.com
+    API_URL = "http://universities.hipolabs.com/search?country=United+States"
+
+    # Send a GET request to the API and convert the response to JSON format
+    data = requests.get(API_URL).json()
+
+    # Return data
+    return data
